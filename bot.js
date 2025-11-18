db0c3bfaf7b091ca0039d404494f97d254410551
BeKuT <158479318+BeKuT@users.noreply.github.com>
2025-11-18 21:14:39 +0500

    bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,6 @@ const RAILWAY_STATIC_URL = process.env.RAILWAY_STATIC_URL;
 const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 const CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 
-
 // Проверка наличия токена
 if (!token) {
     console.error('❌ CRITICAL ERROR: DISCORD_TOKEN not found!');
